4f1a9f7589a839cb622485fec4d45bd60569f03c
matonsjojc <matevz.podlipnik@gmail.com>
2016-10-08 23:22:37 +0200

    paak complete

--- paak/paak.py
+++ paak/paak.py
@@ -32,12 +32,39 @@ def re_ka_pco2(vrstica):
     return False
 
 def re_ka_presezek_baze(vrstica):
-    #sprejme vsebino vrstice, ce ustreza obliki za presezek baze (d+.d), vrne vrednost, sicer False
-    re_ka_presezek_baze = re.compile(r'(?P<ka_presezek_baze>\d+\.\d)')
+    #sprejme vsebino vrstice, ce ustreza obliki za presezek baze (d{1,2}.d), vrne vrednost, sicer False
+    re_ka_presezek_baze = re.compile(r'(?P<ka_presezek_baze>\d{1,2}\.\d)')
     match_ka_presezek_baze = re_ka_presezek_baze.search(vrstica)
     if match_ka_presezek_baze:
         ka_presezek_baze = match_ka_presezek_baze.group('ka_presezek_baze')
         return ka_presezek_baze
+    return False
+
+def re_ka_standardni_hco3(vrstica):
+    #sprejme: vsebino vrstice; ce ustreza obliki za std. hco3 (dd.d), vrne vrednost, sicer False
+    re_ka_standardni_hco3 = re.compile(r'(?P<ka_standardni_hco3>\d\d\.\d)')
+    match_ka_standardni_hco3 = re_ka_standardni_hco3.search(vrstica)
+    if match_ka_standardni_hco3:
+        ka_standardni_hco3 = match_ka_standardni_hco3.group('ka_standardni_hco3')
+        return ka_standardni_hco3
+    return False
+
+def re_sat_o2(vrstica):
+    #sprejme: vsebino vrstice, ce ustreza obliki za sat. o2 (0.[5-9]dd), vrne vredost, sicer False
+    re_sat_o2 = re.compile(r'(?P<sat_o2>0\.[5-9]\d\d)')
+    match_sat_o2 = re_sat_o2.search(vrstica)
+    if match_sat_o2:
+        sat_o2 = match_sat_o2.group('sat_o2')
+        return sat_o2
+    return False
+
+def re_ka_po2(vrstica):
+    #sprejme: vsebino vrstice, ce ustreza obliki za p02(d{1,2}.d)
+    re_ka_po2 = re.compile(r'(?P<ka_po2>\d{1,2}\.\d)')
+    match_ka_po2 = re_ka_po2.search(vrstica)
+    if match_ka_po2:
+        ka_po2 = match_ka_po2.group('ka_po2')
+        return ka_po2
     return False
 
 for report in reports:
@@ -45,94 +72,77 @@ for report in reports:
         doc = slate.PDF(f)
     textFromPage1 = doc[0] #string iz pdfja
     strPO = StringIO(textFromPage1) #StringIO object
-    #print(StringIO.readlines(strPO))
     list_vrstic = StringIO.readlines(strPO) #StringIO object (strPO), pretvorjen v list_vrstic
 
-    #zacetne vrednosti spremenljivk so "ni podatka"
+    #zacetne vrednosti spremenljivk so "ni podatka" ali False
     output = False #string, iz katerega bo nastal txt file
     id_pacienta = "ni podatka"
     priimek_ime = "ni podatka"
     dodatek_kisika = "ni podatka"
     ka_ph = "ni podatka"
     ka_pco2 = "ni podatka"
-    sat_o2 = "ni podatka"
     ka_presezek_baze = "ni podatka"
     ka_standardni_hco3 = "ni podatka"
     sat_o2 = "ni podatka"
     ka_po2 = "ni podatka"
 
-    plinska_prva_vrstica = False
-    ka_ph_stevilka_vrstice = False
-
-    counter = 0 #stevilka vrstice
+    #stevilke vrstic
+    plinska_prva_vrstica = False    #stevilka vrstice, v kateri mora pisati: "Plinska analiza arterijske krvi"
+    ka_ph_stevilka_vrstice = False  #stevilka vrstice, v kateri mora biti Ka-pH
+    counter = 0 #stevilka vrstice (0 = prva), ki je trenutno v obdelavi
 
     for vrstica in list_vrstic:
-        counter += 1
-        #print(counter, ": ", vrstica)
-        numbered_lines += str(counter) + ": " + vrstica
         #preveri, kje se začne izvid PAAK v pdfju; plinska_prva_vrstica = vrstica
         if "Plinska analiza arterijske krvi" in vrstica:
-            print("kle se začne plinska")
             plinska_prva_vrstica = counter
-        #ce najde zacetek paak izvida, 5 - 15 po tem isce, dokler ne najde prve vrednosti, ki ustreza Ka_pH:
-        elif ka_ph == "ni podatka" and plinska_prva_vrstica and (15 > (counter - plinska_prva_vrstica) > 5):
-                if re_ka_ph(vrstica):
-                    ka_ph = re_ka_ph(vrstica)
-                    ka_ph_stevilka_vrstice = counter
-        #ko najde ka_ph, gleda ostale:
+            counter +=1
+        #ce najde zacetek paak izvida, med 5 - 15 vrstic po tem isce, dokler ne najde prve vrednosti, ki ustreza Ka_pH:
+        elif plinska_prva_vrstica and ka_ph == "ni podatka" and (15 > (counter - plinska_prva_vrstica) > 5):
+            if re_ka_ph(vrstica):
+                ka_ph = re_ka_ph(vrstica)
+                ka_ph_stevilka_vrstice = counter
+                counter += 1
+        #ko najde ka_ph, po vrsti izpolnjuje ostale - ce en manjka, ne nadaljuje:
         elif ka_ph_stevilka_vrstice:
             if ka_pco2 == "ni podatka" and re_ka_pco2(vrstica):
                 ka_pco2 = re_ka_pco2(vrstica)
-            elif ka_presezek_baze == "ni podatka" and re_ka_presezek_baze(vrstica):
+            elif ka_pco2 != "ni podatka" and ka_presezek_baze == "ni podatka" and re_ka_presezek_baze(vrstica):
                 ka_presezek_baze = re_ka_presezek_baze(vrstica)
+            elif ka_pco2 != "ni podatka" and ka_presezek_baze != "ni podatka" and ka_standardni_hco3 == "ni podatka" and re_ka_standardni_hco3(vrstica):
+                ka_standardni_hco3 = re_ka_standardni_hco3(vrstica)
+            elif ka_pco2 != "ni podatka" and ka_presezek_baze != "ni podatka" and ka_standardni_hco3 != "ni podatka" and sat_o2 == "ni podatka" and re_sat_o2(vrstica):
+                sat_o2 = re_sat_o2(vrstica)
+            elif ka_pco2 != "ni podatka" and ka_presezek_baze != "ni podatka" and ka_standardni_hco3 != "ni podatka" and sat_o2 != "ni podatka" and ka_po2 == "ni podatka" and re_ka_po2(vrstica):
+                ka_po2 = re_ka_po2(vrstica)
+            counter += 1
+        else:
+            counter += 1
 
-
-    #[:-1] - da je string brez newlina
-    #id in priimek in ime so (verjetno) vedno 6., 11. vrstica
+    #id in priimek in ime so (verjetno) vedno 6., 11. vrstica; [:-1] - da je string brez newlina
     id_pacienta = list_vrstic[7][:-1]
     priimek_ime = list_vrstic[12][:-1]
-    #plinska:
-    dodatek_kisika =list_vrstic[plinska_prva_vrstica + 5][:-1]
-    #ka-ph:
 
-    """
-    ka_ph = list_vrstic[ka_std_hco3_tekst - 84][:-1]
-
-    ka_pco2 = list_vrstic[ka_std_hco3_tekst - 83][:-1]
-    ka_presezek_baze = list_vrstic[ka_std_hco3_tekst - 82][:-1]
-    ka_standardni_hco3 = list_vrstic[ka_std_hco3_tekst - 81][:-1]
-    sat_o2 = list_vrstic[ka_std_hco3_tekst - 78][:-1]
-    ka_po2 = list_vrstic[ka_std_hco3_tekst - 77][:-1]
-    """
-    print("plinska se začne (counter): ", plinska_prva_vrstica)
-
-    print("id: ", id_pacienta)
-    print("priimek in ime: ", priimek_ime)
-    print("vdihana količina kisika: ", dodatek_kisika)
-
-    print("Ka-pH: ", ka_ph)
-    print("Ka-pCO2: ", ka_pco2)
-    print("Ka-presežek baze: ", ka_presezek_baze)
-    print("Ka-standardni HCO3: ", ka_standardni_hco3)
-    print("Ka-saturacija O2: ", sat_o2)
-    print("Ka-pO2: ", ka_po2)
-
+    #vdihana kolicina O2 (% ali l):
+    dodatek_kisika = list_vrstic[plinska_prva_vrstica + 5][:-1]
 
     output = "Priimek in ime: " + priimek_ime + "\n" + \
     "id: " + id_pacienta + "\n" + \
-    "Ka-ph: " + ka_ph + "\n" + \
-    "Ka-pCO2: " + ka_pco2 + "\n" + \
-    "Ka-saturacija O2: " + sat_o2 + "\n"
+    "pH: " + ka_ph + "\n" + \
+    "pCO2: " + ka_pco2 + "\n" + \
+    "presežek baze: " + ka_presezek_baze + "\n" + \
+    "standardni HCO3: " + ka_standardni_hco3 + "\n" + \
+    "saturacija O2: " + sat_o2 + "\n" + \
+    "pO2: " + ka_po2
 
-    #print(output)
+    print(output)
 
     if output:
         outputFileName = priimek_ime + "_" + id_pacienta + ".txt"
-        print(outputFileName)
+        print("zapisujem: ", outputFileName)
         os.chdir(outPath)
         outputFile = open(outputFileName, 'w')
-        #outputFile.write(output)
-        outputFile.write(numbered_lines) #txt file oblike: st. vrstice -1: vrstica"
+        outputFile.write(output)
+        #outputFile.write(numbered_lines) #txt file oblike: st. vrstice -1: vrstica"
         outputFile.close()
     else:
         print("nekisjeban")
